9c2f7c05afb941ac004738dcc525688dadf826d7
Louie Schoenknecht <luisesch97@gmail.com>
2021-05-14 18:17:18 -0700

    add divs to createpost component bc i forgot to

--- src/components/Posts/CreatePost.js
+++ src/components/Posts/CreatePost.js
@@ -3,7 +3,9 @@ import React from 'react';
 function CreatePost(props) {
   return (
     <>
-      <h3>this is a form</h3>
+      <div className='component'>
+        <h3>this is a form</h3>
+      </div>
     </>
   );
 }
