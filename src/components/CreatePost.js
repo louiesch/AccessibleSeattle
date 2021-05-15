a22719473f0bc11c3948e1b01d791c8f4f9f6238
Louie Schoenknecht <luisesch97@gmail.com>
2021-05-14 17:35:15 -0700

    add a home component which will be the parent component of the social feed and new posts

---
+++ src/components/CreatePost.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CreatePost(props) {
+  return (
+    <>
+      <h3>this is a form</h3>
+    </>
+  );
+}
+
+export default CreatePost;
---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import CreatePost from './CreatePost';
+import SocialFeed from './SocialFeed';
+
+class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+
+}
+
+export default Home;
