9564f26540c7485b2694a6372175c587eec049fb
Louie Schoenknecht <luisesch97@gmail.com>
2021-05-16 16:26:54 -0700

    create a pages directory to handle the different windows

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,8 +3,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import App from '../../components/App';
+import reportWebVitals from '../../reportWebVitals';
 
 ReactDOM.render(
   <>
