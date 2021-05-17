46f2df678f1e96512192637babf0a8dc6cc8b5fb
Louie Schoenknecht <luisesch97@gmail.com>
2021-05-17 16:24:02 -0700

    add css styling to format createpost if user is not logged in

--- src/containers/create-post/create-post.js
+++ src/containers/create-post/create-post.js
@@ -1,11 +1,12 @@
-import React from 'react'
-import SignInBtn from '../../components/sign-in-btn/sign-in-btn'
+import React from 'react';
+import './style.css';
+import SignInBtn from '../../components/sign-in-btn/sign-in-btn';
 
 export default function CreatePost() {
   return (
     <div className='createPost'>
       <SignInBtn />
-      <p>to post</p>
+      <p style={{ marginLeft: '10px' }}>to Post!</p>
       
     </div>
   )
