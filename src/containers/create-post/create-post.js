894b152033d3a03543fbb2dfa8c08612cd2e5f84
Louie Schoenknecht <luisesch97@gmail.com>
2021-05-18 10:36:55 -0700

    create and give attributes to text area for creating a post

--- src/containers/create-post/create-post.js
+++ src/containers/create-post/create-post.js
@@ -1,15 +1,26 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import './style.css';
 import SignInBtn from '../../components/sign-in-btn/sign-in-btn';
 import { UserContext } from '../../contexts/user';
 
 export default function CreatePost() {
   const [user, setUser] = useContext(UserContext).user;
+  const [content, setContent] = useState('');
   return (
     <div className='createPost'>
 
       {user ? (
-        <p>Create Post</p> 
+        <div className='loggedIn'>
+          <p>Create Post</p>
+          <div className='loggedInShow'>
+            <textarea
+              className='textarea'
+              rows='10'
+              value={content}
+              onChange={(event) => setContent(event.target.value)}>
+            </textarea>
+          </div>
+        </div>
       ) : (
         <div>
           <SignInBtn />
