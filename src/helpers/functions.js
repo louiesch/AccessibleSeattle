276a0cad789882d862fa9988556bf9a6c7b63453
Louie Schoenknecht <luisesch97@gmail.com>
2021-05-18 15:10:19 -0700

    add a helper function to make post ids, snagged from stack overflow

---
+++ src/helpers/functions.js
@@ -0,0 +1,12 @@
+//snag a function from stackoverflow to assign an id to posts
+
+export default function makeId(length) {
+  let result = '';
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = chcracters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
