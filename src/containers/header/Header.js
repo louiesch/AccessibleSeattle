aba06b81313b9a9802bd655ecfd6bdbd9ddf940d
Louie Schoenknecht <luisesch97@gmail.com>
2021-05-18 09:22:22 -0700

    add code so if user signs in, their profile is shown in top right corner

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,15 +1,21 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import './style.css';
 import SignInBtn from '../../components/sign-in-btn/sign-in-btn';
+import { UserContext } from '../../contexts/user';
 
 function Header() {
 
-  
+  const [user, setUser] = useContext(UserContext).user
 
   return (
     <div className='header'>
       <h1>Accessible Seattle</h1>
-      <SignInBtn />
+        <div className='container'>
+        {user ? 
+        <img className='header-img' 
+        src={user.photoURL} /> 
+        : <SignInBtn />}
+        </div>
     </div>
   );
 }
