be21b85deb16d5930a1d87366eaa464e0b629b06
Louie Schoenknecht <luisesch97@gmail.com>
2021-05-18 10:04:46 -0700

    toggle create post once user is logged in

--- src/containers/create-post/create-post.js
+++ src/containers/create-post/create-post.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './style.css';
 import SignInBtn from '../../components/sign-in-btn/sign-in-btn';
+import { UserContext } from '../../contexts/user';
 
 export default function CreatePost() {
+  const [user, setUser] = useContext(UserContext).user;
   return (
     <div className='createPost'>
-      <SignInBtn />
-      <p style={{ marginLeft: '9px' }}>to Post!</p>
-      
+
+      {user ? (
+        <p>Create Post</p> 
+      ) : (
+        <div>
+          <SignInBtn />
+          <p style={{ marginLeft: '5px' }}>to Post!</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
